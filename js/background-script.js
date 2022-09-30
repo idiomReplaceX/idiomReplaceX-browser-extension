a0a7052aa23a8a6b643414af1485bc4ebf2451d8
Nils Menrad <mail@nils-menrad.de>
2022-09-30 22:37:25 +0200

    Stay active after page change, button on/off

--- js/background-script.js
+++ js/background-script.js
@@ -1,29 +1,73 @@
-browser.browserAction.onClicked.addListener((tab) => {
-  // disable the addond icon button in the active tab
-  browser.browserAction.disable(tab.id);
-  // requires the "tabs" or "activeTab" permission, or host permissions for the URL
-  console.log(tab.url);
+let idiom_active = false;
 
-    function load_attach_script() {
-        let executing = browser.tabs.executeScript(
-            tab.id,
-            {
-                file: 'js/attach-idiomReplaceX.js',
-                allFrames: false
-            }
-        );
-        console.log("attach-idiomReplaceX.js injected");
-        executing.then(
-            function () {
-                // onExecuted
-                console.log("attach-idiomReplaceX.js loaded");
-            },
-            function (error) {
-                // onError
-                console.log("attach-idiomReplaceX.js ERROR:" + error);
-            }
-        );
+/*
+ * Set Script active or inactive when user clicks on icon
+ */
+browser.browserAction.onClicked.addListener((tabId, changeInfo, tab) => {
+    if (!idiom_active) {
+        idiom_active = true;
+        //console.log("idiom aktiviert");
+        restart(tabId.id);
+    } else {
+        idiom_active = false;
+        //console.log("idiom deaktiviert");
+        let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+        gettingActiveTab.then((tabs) => {
+            browser.tabs.update({url: tabs[0].url});
+        });
     }
-
-    load_attach_script();
 });
+
+
+/*
+ Restart for the currently active tab, whenever the user navigates.
+ */
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (!changeInfo.url) {
+        return;
+    }
+    let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+    gettingActiveTab.then((tabs) => {
+        if (tabId == tabs[0].id) {
+            restart(tabId);
+        }
+    });
+});
+
+/*
+ Restart for the currently active tab, whenever background.js is run.
+ */
+let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+gettingActiveTab.then((tabs) => {
+    restart(tabs[0].id);
+});
+
+/*
+ Inject the Script when idiom is active
+ */
+function restart(tabId) {
+    if (idiom_active) {
+        let gettingTab = browser.tabs.get(tabId);
+        gettingTab.then((tab) => {
+            let executing = browser.tabs.executeScript(
+                    tab.id,
+                    {
+                        file: 'js/attach-idiomReplaceX.js',
+                        allFrames: false
+                    }
+            );
+            executing.then(
+                    function () {
+                        // onExecuted
+                        //console.log("attach-idiomReplaceX.js loaded");
+                    },
+                    function (error) {
+                        // onError
+                        console.log("attach-idiomReplaceX.js ERROR:" + error);
+                    }
+            );
+        });
+    } else {
+        // console.log("do nothing ..");
+    }
+}
